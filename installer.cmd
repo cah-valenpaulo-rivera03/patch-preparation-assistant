a0a7ec21616d8edf9e984fd7ed114e8b84ac17d5
cah-valenpaulo-rivera03 <valenpaulo.rivera@cardinalhealth.com>
2025-02-26 15:54:47 -0500

    Updated installer

--- installer.cmd
+++ installer.cmd
@@ -1,5 +1,5 @@
-python -m pip install virtualenv
+python -m pip install virtualenv --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
 python -m virtualenv .venv
 call .venv\Scripts\activate
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
 mkdir output
