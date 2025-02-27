246ee5ffb136de96286b54b3d1b88b35959d5da4
cah-valenpaulo-rivera03 <valenpaulo.rivera@cardinalhealth.com>
2025-02-27 07:56:00 -0500

    fix output cleanup, fix pharma tags, add patchtags.py, add new patch tags, move tpm/gsc stage to week 4

---
+++ patchpreparation/patchtags/patchtags.py
@@ -0,0 +1,198 @@
+week=[
+    {
+        "title": "Non-Prod Week",
+        "header": "Generate Server List",
+        "changes": [
+            {
+                "title": "Change #1",
+                "name": "change1",
+                "patchTags": [
+                    "patch-day-03-at-0000-stage",
+                    "patch-day-03-at-0100-stage",
+                    "patch-day-03-at-0900-stage",
+                    "patch-day-03-at-1200-stage-sap-china",
+                    "patch-day-03-at-1300-stage",
+                    "patch-day-03-at-1400-stage",
+                    "patch-day-03-at-2000-stage",
+                    "patch-day-03-at-2100-stage",
+                    "patch-day-04-at-0200-stage",
+                    "patch-day-04-at-0900-stage-datamarket",
+                    "patch-day-04-at-2000-stage",
+                    "patch-day-04-at-2100-stage",
+                    "patch-day-04-at-2100-stage-b2bi",
+                    "patch-day-04-at-2200-stage",
+                    "patch-day-04-at-2300-stage-b2bi",
+                    "patch-day-05-at-2100-stage",
+                    "patch-day-05-at-2300-stage-b2bi",
+                    "patch-flexible-stage-sap-gsc-primary-app2",
+                    "patch-flexible-stage-sap-singapore",
+                    "patch-flexible-stage-sap-tpm-primary",
+                ]
+            },
+            {
+                "title": "Change #2",
+                "name": "change2",
+                "patchTags": [
+                    "patch-flexible-stage-pds-dr-secondary-manual",
+                    "patch-flexible-stage-pds-primary-manual",
+                    "patch-flexible-stage-sap-nphs-primary-app2",
+                    "patch-flexible-stage-sap-nphs-secondary-app1"
+                ]
+            },
+            {
+                "title": "Change #3",
+                "name": "change3",
+                "patchTags": [
+                    "patch-flexible-prod-sap-gsc-secondary-dr-app1",
+                ]
+            }
+        ]
+    },
+    {
+        "title": "Prod Week 1",
+        "header": "Generate Server List",
+        "changes": [
+            {
+                "title": "Change #1",
+                "name": "change1",
+                "patchTags": [
+                    "patch-day-11-at-1600-prod-df",
+                    "patch-day-11-at-1600-prod-sap-china",
+                    "patch-day-11-at-2100-prod",
+                    "patch-day-11-at-2100-prod-b2bi",
+                    "patch-day-11-at-2300-prod",
+                    "patch-day-11-at-2300-prod-b2bi",
+                    "patch-day-12-at-0000-prod",
+                    "patch-day-12-at-0300-prod",
+                    "patch-day-12-at-2300-prod-b2bi",
+                    "patch-flexible-prod-3pl",
+                    "patch-flexible-prod-hci-manual",
+                    "patch-flexible-prod-matillion-manual",
+                    "patch-flexible-prod-medical-mq",
+                    "patch-flexible-prod-sap-gsc-primary-app2",
+                    "patch-flexible-prod-sap-singapore",
+                    "patch-flexible-prod-sap-tpm-primary-app2",
+                    "patch-flexible-prod-sap-tpm-secondary-dr-app1"
+                ]
+            },
+            {
+                "title": "Change #2",
+                "name": "change2",
+                "patchTags": [
+                    "patch-flexible-stage-pmod2",
+                    "patch-flexible-stage-sap-corp",
+                    "patch-flexible-stage-sap-pharma-primary-app",
+                    "patch-flexible-stage-sap-pharma-secondary"
+                ]
+            },
+            {
+                "title": "Change #3",
+                "name": "change3",
+                "patchTags": [
+                    "patch-flexible-prod-sap-nphs-secondary-app1",
+                    "patch-flexible-prod-sap-nphs-primary-app2-manual",
+                ]
+            }
+        ]
+    },
+    {
+        "title": "Prod Week 2",
+        "header": "Generate Server List",
+        "changes": [
+            {
+                "title": "Change #1",
+                "name": "change1",
+                "patchTags": [
+                    "patch-day-17-at-0000-prod",
+                    "patch-day-18-at-0200-prod",
+                    "patch-day-18-at-0300-prod",
+                    "patch-day-18-at-0300-prod-sas",
+                    "patch-day-18-at-0900-prod-datamarket",
+                    "patch-day-18-at-1500-prod-canada",
+                    "patch-day-18-at-1630-prod-chromeleon",
+                    "patch-day-18-at-1800-prod",
+                    "patch-day-18-at-2000-prod",
+                    "patch-day-18-at-2000-prod-canada",
+                    "patch-day-18-at-2100-bi-monthly-prod-kalik",
+                    "patch-day-18-at-2100-prod",
+                    "patch-day-18-at-2100-prod-Automic",
+                    "patch-day-18-at-2100-prod-canada",
+                    "patch-day-18-at-2200-prod",
+                    "patch-day-18-at-2300-prod",
+                    "patch-day-18-at-2300-prod-cim",
+                    "patch-day-19-at-0000-prod",
+                    "patch-day-19-at-0200-prod",
+                    "patch-day-19-at-0300-prod-maximo",
+                    "patch-day-19-at-0600-prod",
+                    "patch-day-19-at-0600-prod-infosphere",
+                    "patch-day-19-at-2000-prod",
+                    "patch-day-19-at-2100",
+                    "patch-day-19-at-2100-prod",
+                    "patch-day-19-at-2100-prod-Automic",
+                    "patch-day-19-at-2100-prod-canada",
+                    "patch-day-19-at-2200-prod",
+                    "patch-day-19-at-2300-prod",
+                    "patch-flexible-prod-aro-even",
+                    "patch-flexible-prod-aro-odd",
+                    "patch-flexible-prod-pds-dr-secondary-manual",
+                    "patch-flexible-prod-pds-primary-manual",
+                ]
+            },
+            {
+                "title": "Change #2",
+                "name": "change2",
+                "patchTags": [
+                    "patch-day-20-at-0300-prod-moms-day1",
+                    "patch-day-20-at-1100-prod",
+                    "patch-day-21-at-0300-prod-moms-day2"
+                ]
+            },
+            {
+                "title": "Change #3",
+                "name": "change3",
+                "patchTags": [
+                    "patch-day-23-at-1100-prod",
+                ]
+            }
+        ]
+    },
+    {
+        "title": "Prod Week 3",
+        "header": "Generate Server List",
+        "changes": [
+            {
+                "title": "Change #1",
+                "name": "change1",
+                "patchTags": [
+                    "patch-day-25-at-2000-prod",
+                    "patch-day-25-at-2100-prod",
+                    "patch-day-25-at-2100-prod-sap-pharma",
+                    "patch-day-25-at-2200-prod",
+                    "patch-day-25-at-2200-prod-manhattan",
+                    "patch-day-25-at-2200-prod-pmod2-manhattan",
+                    "patch-day-26-at-0000-prod",
+                    "patch-day-26-at-0000-prod-ecm",
+                    "patch-day-26-at-0000-prod-manhattan",
+                    "patch-day-26-at-0100-prod-pmod2-manhattan",
+                    "patch-day-26-at-0200-prod",
+                    "patch-day-26-at-0400-prod-ecm",
+                    "patch-day-26-at-2000-prod",
+                    "patch-day-26-at-2100-prod",
+                    "patch-flexible-prod-ecm-even-manual",
+                    "patch-flexible-prod-ecm-odd-manual",
+                    "patch-flexible-prod-pmod2",
+                    "patch-flexible-prod-sap-corp",
+                    "patch-flexible-prod-sap-pharma",
+                ]
+            },
+            {
+                "title": "Change #2",
+                "name": "change2",
+                "patchTags": [
+                    "patch-flexible-stage-sap-gsc-secondary-app1",
+                    "patch-flexible-stage-sap-tpm-secondary-manual"
+                ]
+            },
+        ]
+    }
+]
